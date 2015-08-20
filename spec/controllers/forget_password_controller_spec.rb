f9182e58990322641ef049b08cabdab72fb29e9c
Chiwen Chen <cwchen2000@gmail.com>
2015-08-20 19:54:16 +0800

    list out test item for create action

--- spec/controllers/forget_password_controller_spec.rb
+++ spec/controllers/forget_password_controller_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 describe PasswordWordController do 
   describe "GET new" do 
-    context "with blank input"
-    context "with valid input"
-    context "with invalid input"
+    context "with blank input" do 
+      it "render forget password page"
+      it "shows the error message 'Please enter your email address' "
+    end
+    context "with valid input" do 
+      it "redirect to sign in page"
+      it "shows the error message 'Your email input is invalid' " 
+    end
+    context "with invalid input" do 
+      it "redirect to reset password page"
+      it "generate the token for that email's owner"
+      it "sends out the email with the right content"
+    end
   end
 end
