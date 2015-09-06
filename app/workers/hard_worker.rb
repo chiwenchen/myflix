9a2045c488758a1a4be9ef764142191bd38ca7db
Chiwen Chen <cwchen2000@gmail.com>
2015-09-06 11:21:54 +0800

    commit

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,6 +1,7 @@
 class HardWorker
   include Sidekiq::Worker
-  def perform(name)
-    Ufe
+  def perform(invitation_id)
+    invitation = Invitation.find(invitation_id)
+    AppMailer.send_invitation_letter(invitation).deliver
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,13 +12,13 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'cwchen2000@gmail.com',
-    user_name:            ENV[gmail_username],
-    password:             ENV[gmail_userpassword],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'myflix.heroku.com',
+  :authentication => :plain
+   }
 
   config.assets.compile = false
 
