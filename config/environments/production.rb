5ecbd72c585309e926ea41884fe8f7796268651d
Chiwen Chen <cwchen2000@gmail.com>
2015-10-12 22:29:29 +0800

    fix production env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,11 +14,11 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :port         => ENV['MAILGUN_SMTP_PORT'],
+  :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'myflix.heroku.com',
+  :domain         => ENV['MAILGUN_DOMAIN'],
   :authentication => :plain
    }
 
