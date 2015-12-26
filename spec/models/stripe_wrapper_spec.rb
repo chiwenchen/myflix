e19886a9f2a8e1e42ebccbcd83bdac3e94faf7fa
Chiwen Chen <cwchen2000@gmail.com>
2015-12-26 11:22:52 +0800

    do firaro install

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -5,7 +5,9 @@ describe StripeWrapper do
     describe ".create" do
       before do 
         StripeWrapper::Charge.set_api_key
+        binding.pry
       end
+
       let (:token) do 
         Stripe::Token.create(
           :card => {
