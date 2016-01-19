9e0e4edbf4d100af4d4c0f7c5feb0ccecdee25a4
Chiwen Chen <cwchen2000@gmail.com>
2016-01-19 13:44:08 +0800

    start stripe subscription plan

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -29,5 +29,23 @@ module StripeWrapper
     def self.set_api_key
       Stripe.api_key = ENV['STRIPE_SECRET_KEY']
     end
+
+    def basic_plan
+      Stripe::Plan.create(
+        :amount => 999,
+        :interval => 'month',
+        :name => 'Monthly pay',
+        :currency => 'usd',
+        :id => 'basic'
+      )
+    end
+
+    def customer(token, user)
+      customer = Stripe::Customer.create(
+        :source => token,
+        :plan => "basic",
+        :email => user.email
+      )
+    end
   end
 end
